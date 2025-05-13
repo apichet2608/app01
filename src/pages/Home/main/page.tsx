b5fb2d97bf9bd1afadd6a15a510a93dca217f43a
apichet <apichet.jtd@outlook.com>
2025-05-13 20:13:44 +0700

    feat: Refactor appbar and home page
    - Updated appbar to remove unnecessary links and adjust layout
    - Modified home page to use a container and display product information
    - Removed the about page and route

--- src/pages/Home/main/page.tsx
+++ src/pages/Home/main/page.tsx
@@ -1,16 +1,11 @@
 const Home = () => {
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Hello
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-semibold">Product : RGPZ-556ML-0A</h1>
+        <div>1</div>
+        <div>2</div>
       </div>
-      <div className="flex justify-center items-center">Hello</div>
     </div>
   );
 };
