36305a9d81ff8eadc279b42d353930b07c83b00e
apichet <apichet.jtd@outlook.com>
2025-05-13 20:17:00 +0700

    Revert "add"
    This reverts commit 89c45f85583b4786cd61a0caf7f6322d96e07446.

--- src/Components/Appbar.tsx
+++ src/Components/Appbar.tsx
@@ -1,5 +1,29 @@
+// import { useNavigate } from "react-router-dom";
+
+// const Appbar = () => {
+//   const navigate = useNavigate();
+
+//   const navigate_to = (path: string) => {
+//     navigate(path);
+//   };
+
+//   return (
+//     <div className="flex gap-2 bg-slate-600 justify-between">
+//       <div className="flex gap-2">
+//         <div className="">😈</div>
+//         <button onClick={() => navigate_to("/")}>Home</button>
+//         <div onClick={() => navigate_to("/about")}>About</div>
+//         <div onClick={() => navigate_to("/dashboard")}>Dashboard</div>
+//       </div>
+//       <div className="flex gap-2">
+//         <div>🏭</div>
+//         <div>👤</div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Appbar;
 import { useNavigate } from "react-router-dom";
-import { Home, Info, LayoutDashboard, Factory, User } from "lucide-react";
 
 const Appbar = () => {
   const navigate = useNavigate();
@@ -9,42 +33,16 @@ const Appbar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a onClick={() => navigate_to("/")}>
-                <Home size={20} /> Home
-              </a>
-            </li>
-            <li>
-              <a onClick={() => navigate_to("/dashboard")}>
-                <LayoutDashboard size={20} /> Dashboard
-              </a>
-            </li>
-          </ul>
-        </div>
-        <a className="font-bold text-2xl">Glass Mask</a>
+    <div className="grid grid-cols-2 gap-4 bg-slate-600 p-2">
+      <div className="grid grid-flow-col gap-4 justify-start">
+        <div className="">😈</div>
+        <button onClick={() => navigate_to("/")}>Home</button>
+        <div onClick={() => navigate_to("/about")}>About</div>
+        <div onClick={() => navigate_to("/dashboard")}>Dashboard</div>
+      </div>
+      <div className="grid grid-flow-col gap-4 justify-end">
+        <div>🏭</div>
+        <div>👤</div>
       </div>
       <div className="navbar-center"></div>
       <div className="navbar-end"></div>
--- src/pages/Home/main/page.tsx
+++ src/pages/Home/main/page.tsx
@@ -1,12 +1,4 @@
 const Home = () => {
-  return (
-    <div>
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold">Product : RGPZ-556ML-0A</h1>
-        <div>1</div>
-        <div>2</div>
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 };
 export default Home;
